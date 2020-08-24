495fa834657f2c28d13fcd41a37a6795ec70e729
Panagiotis Kapsalis <pkapsalis@epu.ntua.gr>
2020-08-24 12:42:49 +0300

    some changes on docker installation

--- app/run.sh
+++ app/run.sh
@@ -1,20 +1,40 @@
 #!/usr/bin/env bash
 
+
+
 check_connectivity() {
+
     echo 'AnalEyeZer is up and Running'
 
+
+
     echo 'Check if PostgreSQL is up...'
+
     while ! nc -z ${POSTGRES_HOST} 5432; do
+
       sleep 0.1
+
     done
+
     echo 'PostgreSQL is up and Running'
+
 }
 
+
+
 if [[ "$PROD_STATUS" = true ]]
+
 then
+
     check_connectivity
+
 fi
 
 
+
+
+
 echo " Run gunicorn"
+
 gunicorn --workers=4 -b 0.0.0.0:5000 wsgi:app --reload
+
